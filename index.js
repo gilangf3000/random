f326ccc5efc0d9e5c62ba31bfd4d65133d6d2efc
gilangf3000 <142327040+gilangf3000@users.noreply.github.com>
2024-01-24 12:20:43 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const func = require('./lib/myfunc')
+
+const app = express();
+const port = 3000;
+
+app.use(cors())
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+};
+
+// Api Nyaaa
+app.get('/api/email',  async (req, res) => {
+  const { email } = await func.getEmail()
+  const { password } = await func.getPassword()
+  res.json({
+    status: 1,
+    email: email,
+    password: password
+  })
+});
+
+app.get('/api/nomer',  async (req, res) => {
+  const { nomer } = await func.getNumber()
+  const { password } = await func.getPassword()
+  res.json({
+    status: 1,
+    nomer: nomer,
+    password: password
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
